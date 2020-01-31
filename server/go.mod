2faf0c140b29f38ea14f3e56d060f1b3d4bddee6
teruya <teru0x01.sheep@gmail.com>
2020-01-31 16:45:57 +0900

    add: gorm

--- server/go.mod
+++ server/go.mod
@@ -3,6 +3,9 @@ module github.com/teru01/image
 go 1.13
 
 require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/jinzhu/gorm v1.9.12
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo/v4 v4.1.14
 	golang.org/x/crypto v0.0.0-20200128174031-69ecbb4d6d5d // indirect
 )
--- server/main.go
+++ server/main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	// _ "github.com/jinzhu/gorm/dialects/"
+	"github.com/teru01/image/server/controller"
+
 	"net/http"
+	"os"
+	"log"
 )
 
 func main() {
+	db := model.ConnectDB()
+
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "hoge, world")
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			cc := &DBContext
+			return next(cc)
+		}
 	})
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig {
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodPost},
+	}))
+
+	e.GET("/", controller.indexGet)
+	e.POST("/hoge", controller.handleHoge)
 	e.Static("/static", "static")
 	e.Logger.Fatal(e.Start(":8888"))
 }
+
+
