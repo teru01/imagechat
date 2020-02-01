3546cbd2857307ac8a6d85fe24111248ca6bf6c3
teruya <teru0x01.sheep@gmail.com>
2020-02-01 17:02:18 +0900

    WIP: add test

---
+++ server/controller/hoge_test.go
@@ -0,0 +1,33 @@
+package controller
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/teru01/image/server/model"
+)
+func TestFetchHoges(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("sqlmock err: %v", err)
+	}
+	defer db.Close()
+
+	mock.ExpectBegin()
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM hoges OFFSET ? LIMIT ?`)
+		.WithArgs(0)
+		.WithArgs(2)
+		.WillReturnRows(NewRows([]string{"id", "name"})
+			.AddRow(1, "aaaaa")
+			.AddRow(2, "bbbbb")
+		)
+	)
+	mock.ExpectCommit()
+
+	e := echo.New()
+	cx := model.DBContext{e.NewContext(), db}
+	err := FetchHoges(cx)
+}
