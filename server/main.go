97d4c56e5cdfd98273eed67250726d21ad29a1ef
teruya <teru0x01.sheep@gmail.com>
2020-02-01 14:51:06 +0900

    refactor: adapt interface

--- server/main.go
+++ server/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/teru01/image/server/controller"
 	"github.com/teru01/image/server/model"
+	"github.com/jinzhu/gorm"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -20,13 +21,17 @@ func main() {
 	e.Use(middleware.BodyDump(func(c echo.Context, reqBody, resBody []byte) {
 		fmt.Fprintf(os.Stderr, "request: %v\n", string(reqBody))
 	}))
-	e.GET("/", func(c echo.Context) error {
-		return controller.IndexGet(&model.DBContext{c, db})
-	})
-	e.POST("/hoge", func(c echo.Context) error {
-		return controller.HandleHoge(&model.DBContext{c, db})
-	})
+	e.GET("/", handlerWrapper(controller.IndexGet, db))
+
+	e.POST("/hoge", handlerWrapper(controller.HandleHoge, db))
 	e.Static("/static", "static")
 	e.Logger.Fatal(e.Start(":8888"))
 }
 
+
+// インタフェースの変換を行う
+func handlerWrapper(f func (c *model.DBContext) error, db *gorm.DB) (func (echo.Context) error) {
+	return func(ec echo.Context) error {
+		return f(&model.DBContext{ec, db})
+	}
+}
