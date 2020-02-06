cbce45dc2287ef3da6c1df18815f7aa73246f423
teruya <teru0x01.sheep@gmail.com>
2020-02-06 11:34:45 +0900

    add: sql test

--- server/controller/hoge_test.go
+++ server/controller/hoge_test.go
@@ -1,40 +1,46 @@
 package controller
 
 import (
-	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"regexp"
 	"testing"
-	
 
-	"github.com/labstack/echo/v4"
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jinzhu/gorm"
+	"github.com/labstack/echo/v4"
 	"github.com/teru01/image/server/model"
 )
 
-var e *echo.echo
-
-func setUp() {
-	e := echo.New()
-
-}
-func TestFetchHoges(t *testing.T) {
+func getDBMock() (*gorm.DB, sqlmock.Sqlmock, error) {
 	db, mock, err := sqlmock.New()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	gdb, err := gorm.Open("mysql", db)
+	if err != nil {
+		return nil, nil, err
+	}
+	return gdb, mock, nil
+}
+
+func TestFetchHoges(t *testing.T) {
+	e := echo.New()
+	db, mock, err := getDBMock()
 	if err != nil {
 		t.Fatalf("sqlmock err: %v", err)
 	}
 	defer db.Close()
 
-	mock.ExpectBegin()
-	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM hoges OFFSET ? LIMIT ?`)
-		.WithArgs(0)
-		.WithArgs(2)
-		.WillReturnRows(NewRows([]string{"id", "name"})
-			.AddRow(1, "aaaaa")
-			.AddRow(2, "bbbbb")
-		)
-	)
-	mock.ExpectCommit()
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `hoges` WHERE `hoges`.`deleted_at` IS NULL LIMIT 2 OFFSET 0")).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "aaaaa").AddRow(2, "bbbbb"))
 
-	cx := model.DBContext{e.NewContext(), db}
-	err := FetchHoges(cx)
+	req := httptest.NewRequest(http.MethodGet, "/hoges?offset=0&limit=2", nil)
+	rec := httptest.NewRecorder()
+	cx := model.DBContext{e.NewContext(req, rec), db}
+	err = FetchHoges(&cx)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
--- server/go.mod
+++ server/go.mod
@@ -3,6 +3,7 @@ module github.com/teru01/image/server
 go 1.13
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.4.1
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/jinzhu/gorm v1.9.12
 	github.com/labstack/echo v3.3.10+incompatible
