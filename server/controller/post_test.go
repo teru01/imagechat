36b729672b258d21599a7fa3463210f9a2ad6de4
teruya <teru0x01.sheep@gmail.com>
2020-07-24 12:32:34 +0900

    extract db test

--- server/controller/post_test.go
+++ server/controller/post_test.go
@@ -2,39 +2,34 @@ package controller
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"testing"
 
-	"github.com/jinzhu/gorm"
-	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	_ "github.com/proullon/ramsql/driver"
+	"github.com/stretchr/testify/assert"
 	"github.com/teru01/image/server/database"
 	"github.com/teru01/image/server/model"
+	"github.com/teru01/image/server/test"
 )
 
-var db *gorm.DB
-
 func TestFetchPost(t *testing.T) {
-	setUp()
-	defer tearDown()
+	db := test.SetUpDB()
+	defer test.TearDownDB(db)
 	e := echo.New()
 
 	target := model.Post{
 		Name:     "bar",
 		ImageUrl: "http://hoge.com/3434",
 	}
-	targetJson := `{"name":"bar", "image_url":"http://hoge.com/3434"}`
-	if err := createSeedData([]model.Post{
-		model.Post{
+	if err := test.CreateSeedData([]model.Creatable{
+		&model.Post{
 			Name:     "hogehoge",
 			ImageUrl: "http://hoge.com/1212",
 		},
-		target,
-		model.Post{
+		&target,
+		&model.Post{
 			Name:     "foor",
 			ImageUrl: "http://hoge.com/5656",
 		},
@@ -53,51 +48,11 @@ func TestFetchPost(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	if rec.Code != http.StatusOK {
-		t.Errorf("unexpected status code. Expected %v got %v", http.StatusOK, rec.Code)
-	}
+	assert.Equal(t, rec.Code, http.StatusOK)
 	var response model.Post
 	if err = json.Unmarshal(rec.Body.Bytes(), &response); err != nil {
 		t.Fatal(err)
 	}
-
-	if response.ImageUrl != target.ImageUrl || response.Name != target.Name {
-		t.Errorf("unexpected response. Expected %v got %v", targetJson, response)
-	}
-}
-
-func createSeedData(items []model.Post, db *gorm.DB) error {
-	for _, i := range items {
-		if err := db.Create(&i).Error; err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func InitializeDB(db *gorm.DB) {
-	db.AutoMigrate(&model.Post{})
-	db.AutoMigrate(&model.User{})
-	db.AutoMigrate(&model.Comment{})
-}
-
-func ResetDB(db *gorm.DB) {
-	db.DropTable(&model.Post{})
-	db.DropTable(&model.User{})
-	db.DropTable(&model.Comment{})
-}
-
-func setUp() {
-	err := godotenv.Load("../.env")
-	if err != nil {
-		log.Fatal(err)
-	}
-	db = database.ConnectDB(os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), "localhost", os.Getenv("MYSQL_TEST_DATABASE"))
-	db.LogMode(true)
-	InitializeDB(db)
-}
-
-func tearDown() {
-	ResetDB(db)
-	db.Close()
+	assert.Equal(t, response.ImageUrl, target.ImageUrl)
+	assert.Equal(t, response.Name, target.Name)
 }
