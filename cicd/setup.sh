8c370119b94da783872872a9d51fae1e85a8d69e
teruya <teru0x01.sheep@gmail.com>
2020-08-07 11:35:05 +0900

    improve setup script

--- cicd/setup.sh
+++ cicd/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -exu
+cd -- "${0%/*}"
 
 NAMESPACE=argocd
 
@@ -15,3 +16,20 @@ kubectl apply -n $NAMESPACE -f argocd-install.yaml
 kubectl create secret generic repository-secret --from-file=.env \
   --dry-run -o yaml | kubectl apply -n $NAMESPACE -f -
 kubectl apply -n $NAMESPACE -f argocd-setup.yaml
+
+while :; do
+  ipaddr=$(kubectl get svc -n "$NAMESPACE" | awk '$2 == "LoadBalancer"{print $4}')
+  if [ "$ipaddr" == "<pending>" ]; then
+    echo -n "."
+    sleep 1
+  else
+    break
+  fi
+done
+
+current_password=$(kubectl get pods -n "$NAMESPACE" -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2)
+
+argocd login "$ipaddr" --username admin --password "$current_password"
+
+new_password=$(awk -F"=" '$1 == "argocd-pass"{print $2}' ./.env)
+argocd account update-password --current-password "$current_password" --new-password "$new_password"
