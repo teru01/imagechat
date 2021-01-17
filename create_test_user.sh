1184130df6524e80db0c0501901f323ba97676b0
teruya <teru0x01.sheep@gmail.com>
2021-01-17 19:25:36 +0900

    update script

--- create_test_user.sh
+++ create_test_user.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-curl -v -X POST -H 'Content-type: application/json' -d '{"name": "hoge", "email": "aaa@example.com", "password": "aaaa1111"}' localhost:8888/api/users
+set -uex
+# json=`printf '{"name": "%s", "email": "%s", "password": "%s"}' $1 $2 $3`
+# echo $json
+curl -v -X POST -H 'Content-type: application/json' -d "`printf '{"name": "%s", "email": "%s", "password": "%s"}' $1 $2 $3`" localhost:8888/api/users
